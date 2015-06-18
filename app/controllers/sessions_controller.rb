eb3eb0af0dc5c61124003d0b73d916196026b47e
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-18 13:22:54 -0400

    adds sessions controller and validations to user email and pass

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,8 @@
+class SessionsController < ApplicationController
+  def index
+  end
+
+  def create
+    # @user = User.find_by()
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+  include BCrypt
+
   has_many :posts
   has_many :comments
+
+  validates :name, length: {minimum: 3}
+  validates :password, length: {minimum: 3}
+  validates :email, presence: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def authenticate(plaintext_password)
+    self.password == plaintext_password
+  end
 end
