7dedf86aec211ab672ad746b04ea7efb99436d39
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-18 12:11:29 -0400

    adds models and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  User.create(name: "marco", email: "marco@marco.net", password_hash: "123")
+  User.first.posts.create(body: "this is a body")
+   User.first.comments.create(body: "this is a comment body")
