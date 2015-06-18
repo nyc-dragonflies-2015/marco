4adf660407e19d5ece49ef211824a9d7c7f2bea2
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-18 19:52:52 -0400

    adds user crud and sessions functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,50 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, only: [:show]
   def show
     @user = User.find_by(id: session[:user_id])
     @posts = @user.posts
     @comments = @user.comments
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "There was a problem with your UserName or Password"
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: session[:user_id])
+  end
+
+  def update
+    # byebug
+    @user = User.find_by(id: session[:user_id])
+    if @user.authenticate(params[:user][:password])
+      @user.update(user_params)
+      redirect_to user_path
+    else
+      flash[:notice] = "Your password was incorrect"
+      redirect_to edit_user_path
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: session[:user_id])
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
