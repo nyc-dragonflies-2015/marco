b05850d7390dbac4eecb5982d93f2490fcf7ee3f
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-18 23:14:20 -0400

    adds url to posts

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150618161002) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
+    t.string   "url",        null: false
     t.string   "body",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
