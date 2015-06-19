602e911845474913fe72307343a87dcfa2f44243
Marc Cardacci <marc.cardacci@gmail.com>
2015-06-19 12:33:05 -0400

    adds unfinished ajax request

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
-  $('')
+  $('.upvote').on('click', upvote)
+});
+
+var upvote = (function(event) {
+  event.preventDefault();
+  console.log($('a.upvote'))
+  // $.ajax({
+  //   url :
+  // })
 });
